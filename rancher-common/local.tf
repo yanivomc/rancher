56edcf8d70edaa5e2042b3ba244e7a5450ff5bf9
yanivomc-k1 <yanivomc@gmail.com>
2020-11-29 21:46:40 +0200

    removed custom node

--- rancher-common/local.tf
+++ rancher-common/local.tf
@@ -6,7 +6,7 @@ resource "local_file" "kube_config_server_yaml" {
   content  = rke_cluster.rancher_cluster.kube_config_yaml
 }
 
-resource "local_file" "kube_config_workload_yaml" {
-  filename = format("%s/%s", path.root, "kube_config_workload.yaml")
-  content  = rancher2_cluster.quickstart_workload.kube_config
-}
+# resource "local_file" "kube_config_workload_yaml" {
+#   filename = format("%s/%s", path.root, "kube_config_workload.yaml")
+#   content  = rancher2_cluster.quickstart_workload.kube_config
+# }
